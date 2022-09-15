cb95c8facb86aaac50f70e0529a872c47f44e982
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-09-15 11:40:35 -0400

    Ajout de la balise de style dans le fichier index.php

--- index.php
+++ index.php
@@ -21,6 +21,11 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Mon WP</title>
+    <style>
+        body {
+            background-color: aquagit;
+        }
+    </style>
 </head>
 <body>
 <main>
