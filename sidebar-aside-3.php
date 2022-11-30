f0301a95885debfed0f6c0e8a6d0bc59791135b4
Pedro Contreras Avendano <pedro.contreras.avendano@gmail.com>
2022-11-30 01:43:43 -0500

    tp2 - Modification des fichiers sidebar pour regler de problemes de nom et de position

--- sidebar-aside-3.php
+++ sidebar-aside-3.php
@@ -1,5 +1,3 @@
 <div id="sidebar-footer-1" class="sidebar">
 	| <?php dynamic_sidebar( 'footer-1' ); ?>
-</div><div id="sidebar-footer-3" class="sidebar">
-	<?php dynamic_sidebar( 'footer-3' ); ?>
 </div>
--- sidebar-footer-4.php
+++ sidebar-footer-4.php
@@ -1,3 +1,3 @@
-<div id="sidebar-footer-3" class="sidebar">
-	<?php dynamic_sidebar( 'footer-3' ); ?>
+<div id="sidebar-footer-4" class="sidebar">
+	<?php dynamic_sidebar( 'footer-4' ); ?>
 </div>
